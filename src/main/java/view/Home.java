22daba37b34d4179d0759eb52b1e7ca191d22da1
ana108 <flutegurl108@hotmail.com>
2015-12-25 17:04:36 -0500

    new changes. Converted project to maven and configured web.xml

--- src/main/java/view/Home.java
+++ src/main/java/view/Home.java
@@ -1,5 +1,12 @@
 package view;
+import javax.faces.bean.ManagedBean;
 
+@ManagedBean(name = "helloWorld", eager = true)
 public class Home {
-
+   public Home() {
+      System.out.println("HelloWorld started!");
+   }
+   public String getMessage() {
+      return "Hello World!";
+   }
 }
